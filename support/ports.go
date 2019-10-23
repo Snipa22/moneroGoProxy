bcd65f2721bce50ed3a06d13169a573713e18f06
Alexander Blair <ablair@snowdozer.io>
2019-10-22 22:51:03 -0700

    Ports-Add: Listener config and initialization.

---
+++ support/ports.go
@@ -0,0 +1,61 @@
+package support
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+)
+
+type Ports struct {
+	Port         uint32 // What port number
+	SSL          bool   // Does the port use SSL?
+	FixedDiff    bool   // Is the port fixed diff
+	StartingDiff uint32 // What's the starting difficulty
+	MaxDiff      uint32 // If the port is fixed diff, this doesn't matter.
+}
+
+func (p *Ports) Listen(c chan<- net.Conn) {
+	var l net.Listener
+	var err error
+	if !p.SSL {
+		l, err = net.Listen("tcp", fmt.Sprintf(":%v", p.Port))
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		certHandle, err := os.Open("server.crt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		certData, err := ioutil.ReadAll(certHandle)
+		if err != nil {
+			log.Fatal(err)
+		}
+		certHandle.Close()
+		keyHandle, err := os.Open("server.key")
+		if err != nil {
+			log.Fatal(err)
+		}
+		keyData, err := ioutil.ReadAll(keyHandle)
+		if err != nil {
+			log.Fatal(err)
+		}
+		keyHandle.Close()
+		cert, err := tls.X509KeyPair(certData, keyData)
+		if err != nil {
+			log.Fatal(err)
+		}
+		cfg := &tls.Config{Certificates: []tls.Certificate{cert}}
+		l, err = tls.Listen("tcp", fmt.Sprintf(":%v", p.Port), cfg)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			c <- nil
+		}
+		c <- conn
+	}
+}
